f7eca6bc3a8793658a25954124ec555d38fc1179
solTheW <patrick.marentine@gmail.com>
2022-10-14 21:38:24 +0200

    Changed URL to BASE_URL

--- src/frontend/state/UserContext.js
+++ src/frontend/state/UserContext.js
@@ -5,7 +5,7 @@ const UserContextProvider = ({ children }) => {
   const [isLoading, setIsLoading] = useState(false)
   const [isAdmin, setIsAdmin] = useState(false)
   const [isLogedIn, setIsLogedIn] = useState(false)
-  const url = `${window.location.origin}`
+  const url = process.env.BASE_URL
 
   const firebaseConfig = {
     apiKey: 'AIzaSyAJJCpqlEjNVWhPVK7Mb_ZBA5ZhhiyAaA4',
