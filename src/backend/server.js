2290dc6c1e4428c175880f8d1f0a4b03358940d7
Patryk Włodarski <patrick.marentine@gmail.com>
2022-10-24 22:03:46 +0200

    Added heroku command

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,13 +1,21 @@
 const db = require('./config/db')
+const path = require('path')
 const express = require('express')
 const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 8080
 
 db.connect()
-app.use(express.json())
-app.use(express.urlencoded())
-app.use(cors())
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('build'))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+  })
+} else {
+  app.use(express.json())
+  app.use(express.urlencoded())
+  app.use(cors())
+}
 
 app.post('/api/login', (req, res) => {
   const { username, password } = req.body
